4998a6cd83509c4695e7d4d2bf025f2eed24a7d0
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-06-21 17:16:53 +0500

    updated, timer for jwt added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { MainPage } from "./pages/main-page/main-page";
 import { ConfigRoutes } from "./routes";
 
 function App() {
+  let hours = 5;
+  let now = new Date().getTime();
+  let setupTime = localStorage.getItem("setupTime");
+  if (setupTime == null) {
+    localStorage.setItem("setupTime", now);
+  } else {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
+      localStorage.clear();
+      localStorage.setItem("setupTime", now);
+    }
+  }
+
   return (
     <div className="App">
       <ConfigRoutes></ConfigRoutes>
